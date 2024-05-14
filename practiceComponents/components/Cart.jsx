586de172141c2d04f505de2d2e193a52eb6c5330
pycusmalaus <pycusmalaus@gmail.com>
2024-05-14 16:58:03 +0530

    UPdated

--- practiceComponents/components/Cart.jsx
+++ practiceComponents/components/Cart.jsx
@@ -1,42 +1,57 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { UsingContext } from "../PracticeConcepts"
 
 export const Cart = () => {
-    const value=useContext(UsingContext)
+    const value=useContext(UsingContext);
+    const [total, setTotal]=useState(0)
     const removeCart=(id)=>{
         value.setCart(value.cart.filter((c)=>c.id !== id))
       }
+      useEffect(()=>{
+        const grandTotal = value.cart
+        let val = 0
+        for(let i=0; i<grandTotal.length;i++){
+            val+=parseInt(grandTotal[i].price)
+            setTotal(val)
+        }
+        if(value.cart.length == 0){
+            setTotal(0);
+        }
+        },[value.cart]);
   return (
-    // <div>{value.cart.map(cart=>(
-    //     <li key={cart.id}>{cart.name}</li>
-    // ))}</div>
+    <>
     <div className="grid grid-cols-5 gap-4">
      {value.cart.map(cart=>(
-        <div className="bg-blue-700 p-6" key={cart.id}>
-          <div className="bg-slate-400 pl-8 p-2">
+        <div className="bg-gray-200 p-6" key={cart.id}>
+          <div className="bg-slate-100 pl-8 p-2">
             <img className="pl-4 w-[180px] h-[180px]" src={cart.image_url} alt="" />
           </div>
-          <div className="flex bg-slate-400">
+          <div className="flex bg-slate-100">
             <p>Name:</p>
             <p>{cart.name}</p>
           </div>
-          <div className="bg-slate-400">
+          <div className="bg-slate-100">
             <p>Description:</p>
             <p>{cart.description}</p>
           </div>
-          <div className="flex bg-slate-400">
+          <div className="flex bg-slate-100">
             <p>Price:</p>
             <p>₹{cart.price}</p>
           </div>
-          <div className="flex bg-slate-400">
+          <div className="flex bg-slate-100">
             <p>Category:</p>
             <p>{cart.category}</p>
           </div>
           <div className="ml-20">
-            <button onClick={()=>removeCart(cart.id)} className="border-2 border-white">Remove from Cart</button>
-          </div>
+            <button onClick={()=>removeCart(cart.id)} className="border-2 border-white bg-red-500 text-white rounded-md p-1">Remove from Cart</button>
+        </div>
         </div>
     ))} 
     </div>
+    <div>
+        Grand Total: {total}
+    </div>
+    
+   </>
   )
 }
--- practiceComponents/components/ProductPage.jsx
+++ practiceComponents/components/ProductPage.jsx
@@ -16,37 +16,32 @@ export const ProductPage = () => {
     <>
     <div className="grid grid-cols-5 gap-4">
      {value.product.map(product=>(
-        <div className="bg-blue-700 p-6" key={product.id}>
-          <div className="bg-slate-400 pl-8 p-2">
+        <div className="bg-gray-200 p-6" key={product.id}>
+          <div className="bg-slate-100 pl-8 p-2">
             <img className="pl-4 w-[180px] h-[180px]" src={product.image_url} alt="" />
           </div>
-          <div className="flex bg-slate-400">
+          <div className="flex bg-slate-100">
             <p>Name:</p>
             <p>{product.name}</p>
           </div>
-          <div className="bg-slate-400">
+          <div className="bg-slate-100">
             <p>Description:</p>
             <p>{product.description}</p>
           </div>
-          <div className="flex bg-slate-400">
+          <div className="flex bg-slate-100">
             <p>Price:</p>
             <p>₹{product.price}</p>
           </div>
-          <div className="flex bg-slate-400">
+          <div className="flex bg-slate-100">
             <p>Category:</p>
             <p>{product.category}</p>
           </div>
           <div className="ml-20">
-            {value.cart && value.cart.includes(product) ? <button onClick={()=>removeCart(product.id)} className="border-2 border-white">Remove from Cart</button>: <button onClick={()=>addCart(product)} className="border-2 border-white bg-gray-400 rounded-md p-1">Add to Cart</button>}
+            {value.cart.includes(product) ? <button onClick={()=>removeCart(product.id)} className="border-2 border-white bg-red-500 text-white rounded-md p-1">Remove from Cart</button>: <button onClick={()=>addCart(product)} className="border-2 border-white bg-blue-950 text-white rounded-md p-1">Add to Cart</button>}
           </div>
         </div>
     ))} 
     </div>
-    {/* {product.cart.map(cart=>{
-      <li key={cart.id}>{cart.name}
-      <p>HI</p>
-      </li>
-    })} */}
    </>
   )
 }
